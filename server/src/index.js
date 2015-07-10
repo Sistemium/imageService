471f997e3cfb63790c6df33d69452d6fa580543b
Albert <kovalevskij.albert@gmail.com>
2015-07-10 18:04:37 +0300

    CORS requests enabled

--- server/src/index.js
+++ server/src/index.js
@@ -4,17 +4,26 @@
 //   })
 var express = require('express')
     , auth = require('./auth')
+    , bodyParser = require('body-parser')
     , multer = require('multer')
     , config = require('../config/config.json')
     , multerConfig = require('../config/multer-config')
     , processRequest = require('./process-request')
     , logErrors = require('./log-errors')
     , app = express()
-    , port = config.applicationPort;
+    , port = config.applicationPort
+    , allowCrossDomain = function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'HEAD,GET,PUT,DELETE,POST,OPTIONS,PATCH');
+        res.header('Access-Control-Allow-Headers', 'Authorization, Start-Page, Page-Size, Content-Type');
+        next();
+    };
 
 app.use(express.static('../../client'));
+app.use(allowCrossDomain);
+app.use(bodyParser.json());
 
-app.post('/api/image/', multer(multerConfig), function (req, res) {
+app.post('/api/image/', auth(), multer(multerConfig), function (req, res) {
   try {
     processRequest(req, res);
   } catch(err) {
