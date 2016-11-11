b68a873d88a94c8ef842e1de4014578d02d05a75
Alexander Levin <alevin.ru@gmail.com>
2016-11-11 23:43:40 +0200

    auth optional

--- server/src/validation/auth.js
+++ server/src/validation/auth.js
@@ -1,29 +1,36 @@
-'use strict';
-
-var config = require('../../config/config.json')
-    , request = require('request');
+const config = require('../../config/config.json');
+const request = require('request');
 
 module.exports = function () {
-    var timestamp = Date.now();
+
+  var timestamp = Date.now();
+  var url = config.auth && config.auth.url;
+
+  if (!url) {
     return function (req, res, next) {
-        var options = {
-            url: config.auth.url,
-            headers: {
-                'Authorization': req.headers.authorization
-            }
-        };
-        request(options, function (error, response, body) {
-            if (error) console.log(error);
-            if (!error && response.statusCode === 200) {
-                timestamp = Date.now();
-                console.log(timestamp + ' info: Body: %s', body);
-                next();
-            } else {
-                timestamp = Date.now();
-                console.log(timestamp + ' info: Response status code %s\nResponse body: %s', response.statusCode, body);
-                res.status(response.statusCode);
-                return res.send(body);
-            }
-        });
+      next()
+    }
+  }
+
+  return function (req, res, next) {
+    var options = {
+      url: url,
+      headers: {
+        'Authorization': req.headers.authorization
+      }
     };
+    request(options, function (error, response, body) {
+      if (error) console.log(error);
+      if (!error && response.statusCode === 200) {
+        timestamp = Date.now();
+        console.log(timestamp + ' info: Body: %s', body);
+        next();
+      } else {
+        timestamp = Date.now();
+        console.log(timestamp + ' info: Response status code %s\nResponse body: %s', response.statusCode, body);
+        res.status(response.statusCode);
+        return res.send(body);
+      }
+    });
+  }
 };
