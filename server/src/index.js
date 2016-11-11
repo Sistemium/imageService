f5009a22d37cda7f0b2648937c07452459944a09
Alexander Levin <alevin.ru@gmail.com>
2016-11-12 00:05:47 +0200

    refactor index.js

--- server/src/index.js
+++ server/src/index.js
@@ -2,42 +2,41 @@
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var express = require('express')
-    , auth = require('./validation/auth')
-    , bodyParser = require('body-parser')
-    , multer = require('multer')
-    , config = require('../config/config.json')
-    , multerConfig = require('../config/multer-config')
-    , processRequest = require('./process-request')
-    , logErrors = require('./log-errors')
-    , getImageByUrl = require('./imageByUrl/get-image-by-url')
-    , app = express()
-    , port = config.applicationPort
-    , allowCrossDomain = function (req, res, next) {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'HEAD,GET,PUT,DELETE,POST,OPTIONS,PATCH');
-        res.header('Access-Control-Allow-Headers', 'Authorization, Start-Page, Page-Size, Content-Type');
-        next();
-    };
+const express = require('express')
+const auth = require('./validation/auth')
+const bodyParser = require('body-parser')
+const multer = require('multer')
+const config = require('../config/config.json')
+const multerConfig = require('../config/multer-config')
+const processRequest = require('./process-request')
+const logErrors = require('./log-errors')
+const getImageByUrl = require('./imageByUrl/get-image-by-url')
+const app = express()
+const port = config.applicationPort
+
+function allowCrossDomain (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'HEAD,GET,PUT,DELETE,POST,OPTIONS,PATCH');
+  res.header('Access-Control-Allow-Headers', 'Authorization,Content-Type');
+  next();
+}
+
+function nextOnErrors (err, req, res, next) {
+  if (err) {
+    res.status(500).json({error: 'Something went wrong'});
+  } else {
+    next();
+  }
+}
 
 app.use(allowCrossDomain);
 app.use(bodyParser.json());
 
-app.post('/api/image/', auth(), multer(multerConfig), processRequest(), logErrors, function (err, req, res, next) {
-    if (err) {
-        res.status(500).send({error: 'Something went wrong...'});
-    } else {
-        next();
-    }
-});
-app.get('/api/image/', auth(), getImageByUrl(), multer(multerConfig), processRequest(), logErrors, function (err, req, res, next) {
-    if (err) {
-        res.status(500).send({error: 'Something went wrong...'});
-    } else {
-        next();
-    }
-});
+const processor = [multer(multerConfig), processRequest(), logErrors, nextOnErrors];
+
+app.post('/api/image', auth(), processor);
+app.get('/api/image', auth(), getImageByUrl(), processor);
 
 app.listen(port, function () {
-    console.log('Server listening on port %d', port);
+  console.log('Server listening on port %d', port);
 });
