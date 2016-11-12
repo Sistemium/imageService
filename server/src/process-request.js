f06b07efb9e86893245944242ca2480687d3aa1a
Alexander Levin <alevin.ru@gmail.com>
2016-11-12 11:12:16 +0200

    autoformat

--- server/src/process-request.js
+++ server/src/process-request.js
@@ -12,80 +12,80 @@ const uuid = require('node-uuid')
 const debug = require('debug')('stm:ims:process-request');
 
 function getResponseAndCleanup(req, res, next) {
-    var dataForUrlFormation = {
-        checksum: req.image.checksum,
-        folder: req.body.folder || req.query.folder
-    };
-    getResponse(req, res, next, dataForUrlFormation)
-        .then(() => cleanupFiles(req.image.folder ,req.image.name, next))
-        .catch(next);
+  var dataForUrlFormation = {
+    checksum: req.image.checksum,
+    folder: req.body.folder || req.query.folder
+  };
+  getResponse(req, res, next, dataForUrlFormation)
+  .then(() => cleanupFiles(req.image.folder ,req.image.name, next))
+  .catch(next);
 }
 
 function processImage(req, res, next) {
-    generateChecksum(req.image.path)
-        .then(checksum => {
-            req.image.checksum = checksum;
-            notAlreadyUploadedOrBadData(req)
-                .then(() => putAllFilesToS3(req, next)
-                    .then(() =>getResponseAndCleanup(req, res, next))
-                    .catch(next))
-                .catch(() => getResponseAndCleanup(req, res, next));
-        })
-        .catch(next);
+  generateChecksum(req.image.path)
+  .then(checksum => {
+    req.image.checksum = checksum;
+    notAlreadyUploadedOrBadData(req)
+    .then(() => putAllFilesToS3(req, next)
+    .then(() =>getResponseAndCleanup(req, res, next))
+    .catch(next))
+    .catch(() => getResponseAndCleanup(req, res, next));
+  })
+  .catch(next);
 }
 
 function checkFormatAndStartProcessing(req, res, next) {
-    checkFormat(req.image)
-      .then(image => {
-        debug('checkFormatAndStartProcessing start');
-        processImage(req, res, next);
-      })
-      .catch(next);
+  checkFormat(req.image)
+  .then(image => {
+    debug('checkFormatAndStartProcessing start');
+    processImage(req, res, next);
+  })
+  .catch(next);
 }
 
 module.exports = function () {
-    return function (req, res, next) {
-        if (req.files.file !== undefined) {
-            debug('Multipart file upload');
-            var image = req.files.file;
-            var folder = config.uploadFolderPath + '/' + uuid.v4();
-            var img = {
-                path: image.path,
-                name: image.name,
-                folder: folder
-            };
-            mkdirp(folder, function () {
-                var image = {
-                    path: img.path,
-                    name: img.name,
-                    folder: img.folder
-                };
-                req.image = image;
-                var writeStream = fs.createWriteStream(folder + '/' + img.name);
-                req.pipe(writeStream);
-                writeStream.on('finish', function () {
-                    checkFormatAndStartProcessing(req, res, next);
-                });
-            });
-        } else if (req.imageFromSrc) {
-            debug('image from src');
-            checkFormatAndStartProcessing(req, res, next);
-        } else {
-            debug('Binary content request');
-            var folder = config.uploadFolderPath + '/' + uuid.v4();
-            var imageName = config.imageInfo.original.name + '.' + config.format;
-            var imagePath = folder + '/' + imageName;
-            mkdirp(folder, () => {
-                var image = {
-                    path: imagePath,
-                    name: imageName,
-                    folder: folder
-                };
-                req.image = image;
-                var writeStream = fs.createWriteStream(imagePath);
-                writeStream.on('finish', () => checkFormatAndStartProcessing(req, res, next));
-                req.pipe(writeStream);
-            });
-        }
+  return function (req, res, next) {
+    if (req.files.file !== undefined) {
+      debug('Multipart file upload');
+      var image = req.files.file;
+      var folder = config.uploadFolderPath + '/' + uuid.v4();
+      var img = {
+        path: image.path,
+        name: image.name,
+        folder: folder
+      };
+      mkdirp(folder, function () {
+        var image = {
+          path: img.path,
+          name: img.name,
+          folder: img.folder
+        };
+        req.image = image;
+        var writeStream = fs.createWriteStream(folder + '/' + img.name);
+        req.pipe(writeStream);
+        writeStream.on('finish', function () {
+          checkFormatAndStartProcessing(req, res, next);
+        });
+      });
+    } else if (req.imageFromSrc) {
+      debug('image from src');
+      checkFormatAndStartProcessing(req, res, next);
+    } else {
+      debug('Binary content request');
+      var folder = config.uploadFolderPath + '/' + uuid.v4();
+      var imageName = config.imageInfo.original.name + '.' + config.format;
+      var imagePath = folder + '/' + imageName;
+      mkdirp(folder, () => {
+        var image = {
+          path: imagePath,
+          name: imageName,
+          folder: folder
+        };
+        req.image = image;
+        var writeStream = fs.createWriteStream(imagePath);
+        writeStream.on('finish', () => checkFormatAndStartProcessing(req, res, next));
+        req.pipe(writeStream);
+      });
     }
+  }
 };
