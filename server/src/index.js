afcd9f89343c256214178d4f2aa7fa1eafae3583
Alexander Levin <alevin.ru@gmail.com>
2016-11-13 15:41:35 +0200

    semicolons

--- server/src/index.js
+++ server/src/index.js
@@ -2,17 +2,17 @@
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const express = require('express')
-const auth = require('./validation/auth')
-const bodyParser = require('body-parser')
-const multer = require('multer')
-const config = require('../config/config.json')
-const multerConfig = require('../config/multer-config')
-const processRequest = require('./process-request')
-const logErrors = require('./log-errors')
-const getImageByUrl = require('./imageByUrl/get-image-by-url')
-const app = express()
-const port = config.applicationPort
+const express = require('express');
+const auth = require('./validation/auth');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const config = require('../config/config.json');
+const multerConfig = require('../config/multer-config');
+const processRequest = require('./process-request');
+const logErrors = require('./log-errors');
+const getImageByUrl = require('./imageByUrl/get-image-by-url');
+const app = express();
+const port = config.applicationPort;
 
 require('debug').log = console.info.bind(console);
 
