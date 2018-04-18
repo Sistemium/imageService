949c19b6578a15c9a684cb613e250f40d80c5ec1
Alexander Levin <alevin.ru@gmail.com>
2018-04-18 13:45:45 +0300

    check-format reject fix

--- server/src/validation/check-format.js
+++ server/src/validation/check-format.js
@@ -5,29 +5,31 @@ const gm = require('gm').subClass({
 const _ = require('lodash');
 const debug = require('debug')('stm:ims:check-format');
 
-module.exports = function(image) {
+module.exports = function (image) {
 
   debug('Checking file format');
 
   return new Promise((resolve, reject) => {
     gm(image.path)
-      .format(function(err, format) {
+      .format(function (err, format) {
 
         if (err) {
-          throw new Error(err);
+          return reject(err)
         }
 
         if (!config.contentTypeFor && !config.contentTypeFor[format.toLowerCase()]) {
-          throw new Error('Incorrect configuration');
+          return reject('Incorrect configuration');
         }
 
-        if (config.contentTypeFor[format.toLowerCase()]) {
-          image.contentType = config.contentTypeFor[format.toLowerCase()];
-          debug('Format is supported, contentType: %s', image.contentType);
-          resolve();
-        } else {
-          reject('Unsupported format');
+        let contentType = config.contentTypeFor[format.toLowerCase()];
+
+        if (!contentType) {
+          return reject('Unsupported format');
         }
+
+        image.contentType = contentType;
+        debug('Format is supported, contentType: %s', image.contentType);
+        resolve();
 
       });
   });
