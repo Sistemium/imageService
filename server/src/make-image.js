d725b3d3786825b665b08db831478b53226af9be
Alexander Levin <alevin.ru@gmail.com>
2020-06-03 07:26:10 +0300

    Update make-image.js

--- server/src/make-image.js
+++ server/src/make-image.js
@@ -9,18 +9,10 @@ export default async function (req, options) {
   const opt = { ...options };
   const width = opt.imageInfo.width || 100;
   const height = opt.imageInfo.height || 100;
-  const { image = {}, key: name = '' } = opt;
+  const { image = {} } = opt;
 
-  // const imagePath = image.path.replace(/(\.jpeg|\.jpg|\.png)$/i, ext => `${name}${ext}`);
-
-  // debug(imagePath, opt);
-
-  const img = sharp(image.path);
-
-  // debug(img);
-
-  const thumbnail = img
-    .resize(width, height)
+  const thumbnail = sharp(image.path)
+    .resize({ width, height, fit: 'inside', withoutEnlargement: true })
     // .max()
     .png();
 
