1ec6333980e8582d9db8d182a9489420f52946ea
Alexander Levin <alevin.ru@gmail.com>
2021-09-09 12:09:47 +0300

    rawFile with mmmagic

--- server/src/rawFile.js
+++ server/src/rawFile.js
@@ -1,18 +1,39 @@
 import FileType from 'file-type';
+import { Magic, MAGIC_MIME_TYPE } from 'mmmagic';
 
+const magic = new Magic(MAGIC_MIME_TYPE);
 const config = require('../config/config.json');
-// const debug = require('debug')('stm:ims:rawFile');
 
-export async function checkIfRaw(image) {
+const debug = require('debug')('stm:ims:rawFile');
 
-  const detect = await FileType.fromFile(image.path);
+export async function checkIfRaw({ path }) {
 
-  if (!detect) {
+  const detect = await FileType.fromFile(path);
+  let { mime } = detect || {};
+
+  if (!mime || mime === 'application/x-cfb') {
+    mime = await getMagicMime(path);
+  }
+
+  if (!mime) {
     return null;
   }
 
-  const ext = config.rawFile[detect.mime]
+  const ext = config.rawFile[mime]
 
-  return ext && { ext, mime: detect.mime };
+  return ext && { ext, mime };
 
+}
+
+async function getMagicMime(path) {
+  return new Promise((resolve, reject) => {
+    magic.detectFile(path, (err, res) => {
+      debug('getMagicMime', res);
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(res);
+    });
+  });
 }
